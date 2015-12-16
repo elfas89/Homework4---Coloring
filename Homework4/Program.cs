57e5c2a2fd29e29c10ae2cc307efa75010701c47
elfas89 <elfas89@gmail.com>
2015-12-16 10:51:08 +0200

    HW4_16122015

---
+++ Homework4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+            person.Name = "Vasya Victim";
+            person.Age = 66;
+
+            Animal animal = new Animal();
+            animal.Breed = "Bird";
+            animal.Legs = 2;
+
+            ColorPrintClass cp = new ColorPrintClass();
+            cp.ColorPrint(person);
+
+            ColorPrintClass ca = new ColorPrintClass();
+            ca.ColorPrint(animal);
+
+        }
+    }
+}
